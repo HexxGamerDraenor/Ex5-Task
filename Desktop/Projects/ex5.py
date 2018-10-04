331061e71f7cba907705f4a70480d631fb079c78
Skyla Moore <s.moore5104@gmail.com>
2018-10-04 11:20:58 +0100

    created print function for reaching 0

--- Desktop/Projects/ex5.py
+++ Desktop/Projects/ex5.py
@@ -1,4 +1,6 @@
 beerBottles = 99
 
-for beerBottles in range(99, 1, -1):
+for beerBottles in range(99, -1, -1):
     print(beerBottles)
+    if(beerBottles == 0):
+        print ("Safety net for beer bottles being 0")
