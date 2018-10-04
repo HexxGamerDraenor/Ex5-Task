1e12e1c618b59f158b660004cb6374bbbecce9cf
Skyla Moore <s.moore5104@gmail.com>
2018-10-04 11:33:08 +0100

    working version, final line corrected, formatted

--- Desktop/Projects/ex5.py
+++ Desktop/Projects/ex5.py
@@ -1,6 +1,4 @@
-beerBottles = 99
-
 for beerBottles in range(99, -1, -1):
-    print(beerBottles + "bottles of beer on the wall, " + beerBottles + "bottles of beer." + "\nTake one down, pass it around, " + beerBottles + "bottles of beer on the wall")
+    print(f"{beerBottles} bottles of beer on the wall, {beerBottles} bottles of beer." + f"\nTake one down, pass it around, {beerBottles} bottles of beer on the wall")
     if(beerBottles == 0):
-        print ("Safety net for beer bottles being 0")
+        print ("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall...")
