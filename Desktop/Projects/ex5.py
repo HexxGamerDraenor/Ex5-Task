7e825ddeef901d9fb3ca1584d482b44dcf6fcb44
Skyla Moore <s.moore5104@gmail.com>
2018-10-04 11:27:44 +0100

    First test with adding repeating lyrics w/ correct number value

--- Desktop/Projects/ex5.py
+++ Desktop/Projects/ex5.py
@@ -1,6 +1,6 @@
 beerBottles = 99
 
 for beerBottles in range(99, -1, -1):
-    print(beerBottles)
+    print(beerBottles + "bottles of beer on the wall, " + beerBottles + "bottles of beer." + "\nTake one down, pass it around, " + beerBottles + "bottles of beer on the wall")
     if(beerBottles == 0):
         print ("Safety net for beer bottles being 0")
